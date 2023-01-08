687010df3373f279dca53e4fb0d62996a79bcdf6
jeici21 <david92_jc@yahoo.es>
2023-01-07 22:26:23 -0500

    Creadas páginas Index y Create.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Index from './pages/Index';
+import Create from './pages/Create';
+import View from './pages/View';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Index />} />
+          <Route path='create' element={<Create />} />
+          <Route path='view/:bookId' element={<View />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Create.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+
+export default function Create() {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [cover, setCover] = useState("");
+    const [intro, setIntro] = useState("");
+    const [completed, setCompleted] = useState(false);
+    const [review, setReview] = useState("");
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        switch (name) {
+            case "title":
+                setTitle(value);
+                break;
+            case "author":
+                setAuthor(value);
+                break;
+            case "intro":
+                setIntro(value);
+                break;
+            case "completed":
+                setCompleted(e.target.checked);
+                break;
+            case "review":
+                setReview(value);
+                break;
+            default:
+        }
+    }
+
+    const handleOnChangeFile = (e) => {
+        const element = e.target;
+        const file = element.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function () {
+            setCover(reader.result.toString());
+        };//obteniendo imagen local para publicarla en el div señalado
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newBook = {
+            id: crypto.randomUUID(), title, author, cover, intro, completed, review
+        };
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>Title</div>
+                    <input type="text" name='title' onChange={handleChange} value={title} />
+                </div>
+                <div>
+                    <div>Author</div>
+                    <input type="text" name='author' onChange={handleChange} value={author} />
+                </div>
+                <div>
+                    <div>Cover</div>
+                    <input type="file" name='cover' onChange={handleOnChangeFile} />
+                    <div>
+                        {!!cover ? <img src={cover} width="200" alt='preview' /> : ""}
+                    </div>{/*si hay imagen disponible la publicará*/}
+                </div>
+                <div>
+                    <div>Introduction</div>
+                    <input type="text" name='intro' onChange={handleChange} value={intro} />
+                </div>
+                <div>
+                    <div>Completed</div>
+                    <input type="checkbox" name='completed' onChange={handleChange} value={completed} />
+                </div>
+                <div>
+                    <div>Review</div>
+                    <input type="text" name='review' onChange={handleChange} value={review} />
+                </div>
+                <input type="submit" value="Register book" />
+            </form>
+        </div>
+    );
+}
